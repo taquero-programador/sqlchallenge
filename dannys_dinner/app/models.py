7ca8a7734a8a4547c9b9c4517006bd993370ad4c
taquero-programador <jrangel93@protonmail.com>
2022-09-05 18:08:36 -0500

    feat(models): create tables on db with model sqlalchemy

--- dannys_dinner/app/models.py
+++ dannys_dinner/app/models.py
@@ -1,14 +1,17 @@
 #!/urs/bin/env python3
 
-from sqlalchemy import Boolean, Column, ForeignKey, String, Integer, Date, DateTime
+from sqlalchemy import (Boolean, Column,
+                        oreignKey, String,
+                        Integer, Date, DateTime)
 from sqlalchemy.orm import relationship
 from database import Base
+from sqlalchemy.dialects import mysql
 
 class Members(Base):
     __tablename__ = "members"
 
     customer_id = Column(String(1), primary_key=True)
-    join_date = Column(Date)
+    join_date = Column(Date, nullable=False)
 
     sales = relationship("Sales", back_populates="memb")
 
@@ -16,9 +19,9 @@ class Members(Base):
 class Menu(Base):
     __tablename__ = "menu"
 
-    product_id = Column(Integer, primary_key=True)
+    product_id = Column(mysql.INTEGER(4), primary_key=True)
     product_name = Column(String(20))
-    price = Column(Boolean)
+    price = Column(Boolean, nullable=False)
 
     sales = relationship("Sales", back_populates="mn")
 
@@ -26,8 +29,9 @@ class Menu(Base):
 class Sales(Base):
     __tablename__ = "sales"
 
+    id = Column(Integer, primary_key=True
     customer_id = Column(String, ForeignKey("members.customer_id"))
-    order_date = Column(DateTime)
+    order_date = Column(DateTime, nullable=False)
     product_id = Column(Integer, ForeignKey("menu.product_id"))
 
     memb = relationship("Members", back_populates="sales")
