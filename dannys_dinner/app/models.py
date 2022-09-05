172bfb30ec18869db40a20c14a965e9086a4ce15
taquero-programador <jrangel93@protonmail.com>
2022-09-05 17:21:37 -0500

    feat(expand models)

--- dannys_dinner/app/models.py
+++ dannys_dinner/app/models.py
@@ -10,3 +10,25 @@ class Members(Base):
     customer_id = Column(String(1), primary_key=True)
     join_date = Column(Date)
 
+    sales = relationship("Sales", back_populates="memb")
+
+
+class Menu(Base):
+    __tablename__ = "menu"
+
+    product_id = Column(Integer, primary_key=True)
+    product_name = Column(String(20))
+    price = Column(Boolean)
+
+    sales = relationship("Sales", back_populates="mn")
+
+
+class Sales(Base):
+    __tablename__ = "sales"
+
+    customer_id = Column(String, ForeignKey("members.customer_id"))
+    order_date = Column(DateTime)
+    product_id = Column(Integer, ForeignKey("menu.product_id"))
+
+    memb = relationship("Members", back_populates="sales")
+    mn = relationship("Menu", back_populates="sales")
