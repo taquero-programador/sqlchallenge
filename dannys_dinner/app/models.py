6682e9d79b22e6da2c311ac1a04335ba352dee0a
taquero-programador <jrangel93@protonmail.com>
2022-09-06 11:09:43 -0500

    feat(relation)

--- dannys_dinner/app/models.py
+++ dannys_dinner/app/models.py
@@ -8,25 +8,28 @@ from database import Base
 from sqlalchemy.dialects import mysql
 
 class Members(Base):
+
     __tablename__ = "members"
 
     customer_id = Column(String(1), primary_key=True)
     join_date = Column(Date, nullable=False)
 
-    sales = relationship("Sales", back_populates="memb")
+    sales = relationship("Sales")
 
 
 class Menu(Base):
+
     __tablename__ = "menu"
 
     product_id = Column(mysql.INTEGER(4), primary_key=True)
     product_name = Column(String(20))
     price = Column(Boolean, nullable=False)
 
-    sales = relationship("Sales", back_populates="mn")
+    sales = relationship("Sales")
 
 
 class Sales(Base):
+
     __tablename__ = "sales"
 
     id = Column(Integer, primary_key=True
@@ -34,5 +37,3 @@ class Sales(Base):
     order_date = Column(DateTime, nullable=False)
     product_id = Column(Integer, ForeignKey("menu.product_id"))
 
-    memb = relationship("Members", back_populates="sales")
-    mn = relationship("Menu", back_populates="sales")
