94c2ed9896ae02fe6db43905a27f6127ff258b7b
taquero-programador <jrangel93@protonmail.com>
2022-09-06 22:57:54 +0000

    fix: response_model

--- dannys_dinner/app/database.py
+++ dannys_dinner/app/database.py
@@ -3,7 +3,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import settings
+from .config import settings
 
 
 SQLALCHEMY_DATEBASE_URL = "mysql+pymysql://{}:{}@{}/bender?charset=utf8mb4"
--- dannys_dinner/app/main.py
+++ dannys_dinner/app/main.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
-import models as md
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
-from . import models, schemas
-from database import SessionLocal, engine
+from . import models, schemas, crud
+from .database import SessionLocal, engine
 
-md.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -18,9 +17,9 @@ def get_db():
         db.close()
 
 
-@app.post("/members/", reponse_models=schemas.Members)
+@app.post("/members/")
 def create_members(mem: schemas.Members, db: Session = Depends(get_db)):
     db_user = crud.validate_user(db, id_user=mem.customer_id)
     if db_user:
-        raise HTTPException(status_code=400, datail="Member already exist")
+        raise HTTPException(status_code=400, detail="Member already exist")
     return crud.create_member(db=db, user=mem)
