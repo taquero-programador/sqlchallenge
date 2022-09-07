9e0e987ba805bb8c39205f08d7f65f596e5511c8
taquero-programador <jrangel93@protonmail.com>
2022-09-07 12:12:21 -0500

    fix: pep

--- dannys_dinner/app/crud.py
+++ dannys_dinner/app/crud.py
@@ -7,6 +7,7 @@ from . import models, schemas
 def get_user(db: Session, user_id: str):
     return db.query(models.Members).filter(models.Members.customer_id == user_id).first()
 
+
 def get_all_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Members).offset(skip).limit(limit).all()
 
@@ -17,6 +18,7 @@ def create_member(db: Session, user: schemas.MembersCreate):
     db.commit()
     db.refresh(db_user)
     return db_user
+
 
 def validate_user(db: Session, user_id: str):
     return db.query(models.Members).filter(models.Members.customer_id==user_id).first()
